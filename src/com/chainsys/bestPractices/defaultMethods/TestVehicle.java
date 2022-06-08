3f983b28e4d1e20c69a3c3b5179f37b6953b9b7e
Omkumar T R <101443243+OmkumarTR@users.noreply.github.com>
2022-06-08 10:46:45 +0530

    first commit

--- src/com/chainsys/bestPractices/defaultMethods/TestVehicle.java
+++ src/com/chainsys/bestPractices/defaultMethods/TestVehicle.java
@@ -3,8 +3,14 @@ package com.chainsys.bestPractices.defaultMethods;
 public class TestVehicle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Car firstCar = new Car();
+		firstCar.start();
+		firstCar.move();
+		
+		Bike firstBike = new Bike();
+		firstBike.start();
+		firstBike.move();
 	}
 
 }
